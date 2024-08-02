4c104c1be19d87109ae38315e55fd41a22c13fb9
rafaelluz10 <rafael.luztp1@gmail.com>
2024-08-02 19:49:42 -0300

    Criando Tabelas

--- dbTeste.sql
+++ dbTeste.sql
@@ -11,3 +11,23 @@ use dbTeste;
 
 -- visualizando banco de dados -- 
 show databases; 
+
+-- criando tabelas no banco de dados -- 
+create table tbUsuarios(
+codUsu int not null auto_increment, 
+nome varchar(50) not null, 
+senha varchar(10) not null, 
+primary key(codUsu) 
+); 
+
+create table tbFuncionarios(
+codFunc int not null auto_increment,
+nome varchar(100) not null, 
+email varchar(100), 
+telCel char(10), 
+);
+-- visualizando tabelas -- 
+show tables; 
+
+-- visualizando estrutura das tabelas -- 
+desc tbUsuarios; 

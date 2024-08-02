fba42a0e66f04143a37301570e3180e4c3f0bd8e
rafaelluz10 <rafael.luztp1@gmail.com>
2024-08-02 20:16:57 -0300

    Criando Chave Estrangeira

--- dbTeste.sql
+++ dbTeste.sql
@@ -13,21 +13,28 @@ use dbTeste;
 show databases; 
 
 -- criando tabelas no banco de dados -- 
-create table tbUsuarios(
-codUsu int not null auto_increment, 
-nome varchar(50) not null, 
-senha varchar(10) not null, 
-primary key(codUsu) 
-); 
-
 create table tbFuncionarios(
 codFunc int not null auto_increment,
 nome varchar(100) not null, 
 email varchar(100), 
 telCel char(10), 
+cpf char(14), 
+primary key(codFunc)
 );
+
+create table tbUsuarios(
+codUsu int not null auto_increment, 
+nome varchar(50) not null, 
+senha varchar(10) not null,
+codFunc int not null,  
+primary key(codUsu), 
+foreign key(codFunc)references tbFuncionarios(codFunc)
+); 
+
+
 -- visualizando tabelas -- 
 show tables; 
 
 -- visualizando estrutura das tabelas -- 
-desc tbUsuarios; 
+desc tbUsuarios;
+desc tbFuncionarios;  

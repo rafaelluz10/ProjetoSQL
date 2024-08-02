eab7e395fbf891e392d8504fe95a4b5b77be3e78
rafaelluz10 <rafael.luztp1@gmail.com>
2024-08-02 19:28:26 -0300

    Script Sql Teste

---
+++ dbTeste.sql
@@ -0,0 +1,13 @@
+-- Testando SQL -- 
+
+-- apagar o banco de dados -- 
+drop database dbTeste;
+
+-- criar Banco de Dados -- 
+create database dbTeste; 
+
+-- acessar o banco de dados -- 
+use dbTeste; 
+
+-- visualizando banco de dados -- 
+show databases; 
